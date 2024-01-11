6609f3db6214d103f4d4f39b17b7e3d8da66b1be
M10mo <mohdkanoongo@gmail.com>
2024-01-10 21:03:25 -0500

    changes

---
+++ pokemon-search/src/main.js
@@ -0,0 +1,30 @@
+import './style.css';
+import { fetchData } from './utils/utils.js';
+
+const main = async () => {
+  console.log('testing');
+  const onePoke = await fetchData('https://pokeapi.co/');
+  const div = document.createElement('div');
+  div.innterHTML
+
+}
+
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//     <a href="https://vitejs.dev" target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+//     </a>
+//     <h1>Hello Vite!</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite logo to learn more
+//     </p>
+//   </div>
+// `
+
+// setupCounter(document.querySelector('#counter'))
---
+++ pokemon-search/src/utils/render.js
@@ -0,0 +1,7 @@
+import { fetchData } from './utils.js'
+
+const fetchPoke = fetch('https://pokeapi.co/');
+
+console.log(onePoke);
+
+
